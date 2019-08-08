0de0e8b663bcd44fef373eb88fd2ec1a6403879e
Stephen Ord <steve.ord@gmail.com>
2019-08-08 15:55:13 +1000

    Added the dependencies macro

---
+++ dependencies.cmake
@@ -0,0 +1,12 @@
+macro(install_dependencies)
+
+if(NOT EXISTS "${CMAKE_SOURCE_DIR/dependencies.txt")
+    message(FATAL_ERROR "Cannot find dependencies list")
+endif())
+
+file(READ "${CMAKE_SOURCE_DIR/dependencies.txt" depends)
+string(REGEX REPLACE "\n" ";" depends "${depends}")
+foreach(dependency ${depends})
+    message(STATUS finding ${dependency})
+endforeach(dependency)
+
