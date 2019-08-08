4e6eb3b1631ddd9b3aba00f285aedf049ab39635
Stephen Ord <steve.ord@gmail.com>
2019-08-08 16:07:45 +1000

    More syntax errors

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,10 +1,10 @@
 macro(install_dependencies)
 
-if(NOT EXISTS "${CMAKE_SOURCE_DIR/dependencies.txt")
-    message(FATAL_ERROR "Cannot find dependencies list")
+if(NOT EXISTS "${CMAKE_SOURCE_DIR}/dependencies.txt")
+    message(FATAL_ERROR "Cannot find dependencies.txt")
 endif()
 
-file(READ "${CMAKE_SOURCE_DIR/dependencies.txt" depends)
+file(READ "${CMAKE_SOURCE_DIR}/dependencies.txt" depends)
 string(REGEX REPLACE "\n" ";" depends "${depends}")
 foreach(dependency ${depends})
     message(STATUS finding ${dependency})
