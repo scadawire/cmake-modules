a9672bfe7a764776481028a136cedbd3d369a5f5
Stephen Ord <steve.ord@gmail.com>
2019-08-08 16:03:35 +1000

    a typo in the dependencies macro

--- dependencies.cmake
+++ dependencies.cmake
@@ -2,7 +2,7 @@ macro(install_dependencies)
 
 if(NOT EXISTS "${CMAKE_SOURCE_DIR/dependencies.txt")
     message(FATAL_ERROR "Cannot find dependencies list")
-endif())
+endif()
 
 file(READ "${CMAKE_SOURCE_DIR/dependencies.txt" depends)
 string(REGEX REPLACE "\n" ";" depends "${depends}")
